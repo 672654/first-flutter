9e86f2425d77030504e57d2b5563589629f8b757
672654 <henrik_krane@hotmail.com>
2026-08-01 23:51:10 +0200

    supabase service updated with CRUD

--- lib/data/services/supabase_service/supabase_service.dart
+++ lib/data/services/supabase_service/supabase_service.dart
@@ -15,4 +15,31 @@ class SupabaseService {
     
     return response;
     }
+
+  Future<void> createGear(Map<String, dynamic> newGear) async {
+    final response = await _client
+        .from('gear')
+        .insert(newGear);
+
+    return response;
   }
+
+  Future<void> updateGear(String id, Map<String, dynamic> updatedGear) async {
+    final response = await _client
+        .from('gear')
+        .update(updatedGear)
+        .eq('id', id);
+
+    return response;
+  }
+
+  Future<void> deleteGear(String id) async {
+    final response = await _client
+        .from('gear')
+        .delete()
+        .eq('id', id);
+
+    return response;
+  }
+
+}
