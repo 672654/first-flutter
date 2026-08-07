4f3426e8aca05fc0b889913d220c494ecf381c06
672654 <henrik_krane@hotmail.com>
2026-08-07 22:17:19 +0200

    gear_screen now show by type

--- lib/presentation/features/gear_view/viewmodel/gear_cubit.dart
+++ lib/presentation/features/gear_view/viewmodel/gear_cubit.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_supabase_pack/data/repositories/gear_repo/gear_repository_interface.dart';
+import 'package:flutter_supabase_pack/domain/models/gear.dart';
 import 'package:flutter_supabase_pack/presentation/features/gear_view/viewmodel/gear_state.dart';
 
 class GearCubit extends Cubit<GearState> {
@@ -12,8 +13,20 @@ class GearCubit extends Cubit<GearState> {
   Future<void> loadAllGear() async {
     emit(const GearLoading());
     try {
+      //Hent alle gear i en liste
       final gearList = await _repo.getAllGear();
-      emit(GearLoaded(gearList));
+
+      //opprett en map for å gruppere gear etter type
+      final Map<String, List<Gear>> gearByType = {};
+
+      //Gruppere gear etter type
+      for(final gear in gearList) {
+        final upperCaseFirstLetter = gear.type.name[0].toUpperCase() + gear.type.name.substring(1);
+        gearByType.putIfAbsent(upperCaseFirstLetter, () => []).add(gear);
+      }
+
+      emit(GearLoaded(gearByType));
+
     } catch (e) {
       emit(GearError(e.toString()));
     }
